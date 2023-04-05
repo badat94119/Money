146d6768360fc0030a6c77c5d66326418d64aff5
badat94119 <datba411@gmail.com>
2023-04-06 01:37:42 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("USD = ");
+            string value = Console.ReadLine()!;
+            int price = Convert.ToInt32(value) * 23000;
+            Console.WriteLine("VND = " + price);
+        }
+    }
+}
